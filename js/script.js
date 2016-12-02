e72f38029a918bfc36adb930dcad35a9da476a5f
Eric Snell <eric_snell21@yahoo.com>
2016-12-02 11:36:09 -0500

    each box with different draggable state, more styling

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 $((() => {
 
-  
+  $('.box').draggable();
+
+  // Content draggable anywhere; container will scroll with fixed height/width
+  $('#box1').draggable({ scroll: true });
+
+  // Content only draggable vertically
+  $('#box2').draggable({ axis: 'x' });
+
+  // Content only draggable horizontally
+  $('#box3').draggable({ axis: 'y'});
+
+  // Content draggable within certain area ('parent' or specific element)
+  $('#box4').draggable({ containment: '.container' });
+
+
+
+
 
 }));
