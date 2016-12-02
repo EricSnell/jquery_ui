a9ad6c856aef3289cc1777fbdf24240069d1b263
Eric Snell <eric_snell21@yahoo.com>
2016-12-02 11:43:43 -0500

    added droppable.html

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 $((() => {
 
-  $('.box').draggable();
+  // DRAGGABLE
 
+  $('.box').draggable();
   // Content draggable anywhere; container will scroll with fixed height/width
   $('#box1').draggable({ scroll: true });
-
   // Content only draggable vertically
   $('#box2').draggable({ axis: 'x' });
-
   // Content only draggable horizontally
   $('#box3').draggable({ axis: 'y'});
-
   // Content draggable within certain area ('parent' or specific element)
   $('#box4').draggable({ containment: '.container' });
 
-
+  // DROPPABLE
+  
 
 
 
